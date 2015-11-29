782ed7c500c4efcc221f2245d6a5be33075e41a2
Demigiant <daniele.giardini@gmail.com>
2015-11-29 15:16:29 +0100

    ScenesPanel now allows right-click to ping scene + drag & drop scene to add them to build list

--- _Demigiant.Libraries/DeEditorTools/ScenesPanel/ScenesPanel.cs
+++ _Demigiant.Libraries/DeEditorTools/ScenesPanel/ScenesPanel.cs
@@ -2,6 +2,7 @@
 // Created: 2015/11/29 13:40
 // License Copyright (c) Daniele Giardini
 
+using System.Collections.Generic;
 using DG.DemiEditor;
 using System.IO;
 using System.Text;
@@ -53,7 +54,12 @@ namespace DG.DeEditorTools.ScenesPanel
                 string sceneName = Path.GetFileNameWithoutExtension(scene.path);
                 scene.enabled = EditorGUILayout.Toggle(scene.enabled, GUILayout.Width(16));
                 if (GUILayout.Button(sceneName, DeGUI.styles.button.tool.Add(TextAnchor.MiddleLeft))) {
-                    if (EditorApplication.SaveCurrentSceneIfUserWantsTo()) {
+                    if (Event.current.button == 1) {
+                        // Right-click: ping scene in Project panel
+                        Object sceneObj = AssetDatabase.LoadAssetAtPath<Object>(scene.path);
+                        EditorGUIUtility.PingObject(sceneObj);
+                    } else if (EditorApplication.SaveCurrentSceneIfUserWantsTo()) {
+                        // Left-click: open scene
                         EditorApplication.OpenScene(scene.path);
                     }
                 }
@@ -61,7 +67,52 @@ namespace DG.DeEditorTools.ScenesPanel
             }
             if (GUI.changed) EditorBuildSettings.scenes = scenes;
 
+            // Drag drop area
+            GUILayout.Space(8);
+            DrawDragDropSceneArea();
+
             GUILayout.EndScrollView();
+        }
+
+        void DrawDragDropSceneArea()
+        {
+            Event e = Event.current;
+            Rect dropRect = GUILayoutUtility.GetRect(0, 44, GUILayout.ExpandWidth(true));
+            dropRect.x += 3;
+            dropRect.y += 3;
+            dropRect.width -= 6;
+            EditorGUI.HelpBox(dropRect, "Drop Scenes here to add them to the build list", MessageType.Info);
+
+            switch (e.type) {
+            case EventType.DragUpdated:
+            case EventType.DragPerform:
+                if (!dropRect.Contains(e.mousePosition)) return;
+                bool isValid = true;
+                // Verify if drop is valid (contains only scenes)
+                foreach (Object dragged in DragAndDrop.objectReferences) {
+                    if (!dragged.ToString().EndsWith(".SceneAsset)")) {
+                        // Invalid
+                        DragAndDrop.visualMode = DragAndDropVisualMode.Rejected;
+                        isValid = false;
+                        break;
+                    }
+                }
+                if (!isValid) return;
+                DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
+                if (e.type == EventType.DragPerform) {
+                    // Add scenes
+                    DragAndDrop.AcceptDrag();
+                    EditorBuildSettingsScene[] currScenes = EditorBuildSettings.scenes;
+                    List<EditorBuildSettingsScene> newScenes = new List<EditorBuildSettingsScene>(currScenes.Length + DragAndDrop.objectReferences.Length);
+                    foreach (EditorBuildSettingsScene s in currScenes) newScenes.Add(s);
+                    foreach (Object dragged in DragAndDrop.objectReferences) {
+                        EditorBuildSettingsScene scene = new EditorBuildSettingsScene(AssetDatabase.GetAssetPath(dragged), true);
+                        newScenes.Add(scene);
+                    }
+                    EditorBuildSettings.scenes = newScenes.ToArray();
+                }
+                break;
+            }
         }
     }
 }
