c8dd5777dee8e923546832e0660cdd9e84bfc5e6
Demigiant <daniele.giardini@gmail.com>
2015-12-04 15:23:01 +0100

    Added GameObject.AwakeAndDeactivateExtension

---
+++ _Demigiant.Libraries/DeExtensions/GameObjectExtensions.cs
@@ -0,0 +1,22 @@
+﻿// Author: Daniele Giardini - http://www.demigiant.com
+// Created: 2015/12/04 15:08
+// License Copyright (c) Daniele Giardini
+
+using UnityEngine;
+
+namespace DG.DeExtensions
+{
+    public static class GameObjectExtensions
+    {
+        /// <summary>
+        /// Activates then immediately deactivates the target gameObject.
+        /// Useful when wanting to call Awake before deactivating a gameObject.
+        /// </summary>
+        /// <param name="go"></param>
+        public static void AwakeAndDeactivate(this GameObject go)
+        {
+            go.SetActive(true);
+            go.SetActive(false);
+        }
+    }
+}
