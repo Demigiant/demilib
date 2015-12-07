23994291de035a93454bf88d4c9197d8b73f4f0d
Demigiant <daniele.giardini@gmail.com>
2015-12-07 18:37:20 +0100

    Marked DeAudioCollection as deprecated (better to use DeAudioClipData object in custom classes)

--- _Demigiant.Libraries/DeAudio/DeAudioCollection.cs
+++ _Demigiant.Libraries/DeAudio/DeAudioCollection.cs
@@ -2,6 +2,7 @@
 // Created: 2015/11/24 16:26
 // License Copyright (c) Daniele Giardini
 
+using System;
 using UnityEngine;
 
 namespace DG.DeAudio
@@ -9,6 +10,7 @@ namespace DG.DeAudio
     /// <summary>
     /// A collection of audio files. Still needs to be completed.
     /// </summary>
+    [Obsolete("This was a test, but using DeAudioClipData inside custom classes is much better in my opinion, so I abandoned it")]
     public class DeAudioCollection : MonoBehaviour
     {
         public DeAudioClipData[] data;
