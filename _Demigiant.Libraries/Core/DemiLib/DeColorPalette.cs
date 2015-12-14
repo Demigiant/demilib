d711a343526530b9bfcb40105960deb5d8f8edda
Demigiant <daniele.giardini@gmail.com>
2015-12-14 15:53:32 +0100

    Nicer GUI colors

--- _Demigiant.Libraries/Core/DemiLib/DeColorPalette.cs
+++ _Demigiant.Libraries/Core/DemiLib/DeColorPalette.cs
@@ -25,7 +25,8 @@ namespace DG.DemiLib
     public class DeColorBG
     {
         public DeSkinColor def = Color.white;
-        public DeSkinColor critical = Color.red;
+//        public DeSkinColor critical = Color.red;
+        public DeSkinColor critical = new DeSkinColor(new Color(0.9411765f, 0.2388736f, 0.006920422f, 1f), new Color(1f, 0.2482758f, 0f, 1f));
         public DeSkinColor divider = new DeSkinColor(new Color(0.5f, 0.5f, 0.5f, 1), Color.black);
         public DeSkinColor toggleOn = Color.green;
         public DeSkinColor toggleOff = Color.white;
@@ -38,7 +39,8 @@ namespace DG.DemiLib
     public class DeColorContent
     {
         public DeSkinColor def = new DeSkinColor(Color.black, new Color(0.7f, 0.7f, 0.7f, 1));
-        public DeSkinColor critical = new DeSkinColor(new Color(1f, 0.8458418f, 0.4411765f, 1f), new Color(1f, 0.1691176f, 0.1691176f, 1f));
+//        public DeSkinColor critical = new DeSkinColor(new Color(1f, 0.8458418f, 0.4411765f, 1f), new Color(1f, 0.1691176f, 0.1691176f, 1f));
+        public DeSkinColor critical = new DeSkinColor(new Color(1f, 0.9148073f, 0.5588235f, 1f), new Color(1f, 0.3881846f, 0.3014706f, 1f));
         public DeSkinColor toggleOn = new DeSkinColor(new Color(1f, 0.9686275f, 0.6980392f, 1f), new Color(0.8117647f, 1f, 0.5607843f, 1f));
         public DeSkinColor toggleOff = new DeSkinColor(new Color(0.3529412f, 0.3647059f, 0.3647059f, 1f), new Color(0.5294118f, 0.5294118f, 0.5294118f, 1f));
     }
