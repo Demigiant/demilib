08d41108ea7e049a20e2c509c86d0591072ae48f
Demigiant <daniele.giardini@gmail.com>
2016-03-01 12:39:31 +0100

    Added StringExtensions

---
+++ _Demigiant.Libraries/DeExtensions/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿// Author: Daniele Giardini - http://www.demigiant.com
+// Created: 2016/03/01 12:29
+// License Copyright (c) Daniele Giardini
+namespace DG.DeExtensions
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Returns TRUE if the string is null or empty
+        /// </summary>
+        /// <param name="trimSpaces">If TRUE (default) and the string contains only spaces, considers it empty</param>
+        /// <returns></returns>
+        public static bool IsNullOrEmpty(this string s, bool trimSpaces = true)
+        {
+            if (s == null) return true;
+            return trimSpaces ? s.Trim().Length == 0 : s.Length == 0;
+        }
+    }
+}
