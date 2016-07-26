c85ccfa2c0560f3f01b8c35b147a7923ecd5e18b
Demigiant <daniele.giardini@gmail.com>
2016-07-26 14:37:33 +0200

    DeEditorPrefabUtils > Added BreakPrefab methods

--- _Demigiant.Libraries/Core/DemiEditor/DeEditorPrefabUtils.cs
+++ _Demigiant.Libraries/Core/DemiEditor/DeEditorPrefabUtils.cs
@@ -2,6 +2,7 @@
 // Created: 2016/02/16 18:36
 // License Copyright (c) Daniele Giardini
 
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
@@ -19,6 +20,32 @@ namespace DG.DemiEditor
         public static void ApplyPrefabInstanceModifications(GameObject instance)
         {
             PrefabUtility.ReplacePrefab(instance, PrefabUtility.GetPrefabParent(instance), ReplacePrefabOptions.ConnectToPrefab);
+        }
+
+        /// <summary>
+        /// Completely removes any prefab connection from the given prefab instances.
+        /// Based on RodGreen's method (http://forum.unity3d.com/threads/82883-Breaking-connection-from-gameObject-to-prefab-for-good.?p=726602&amp;viewfull=1#post726602)
+        /// </summary>
+        public static void BreakPrefabInstances(List<GameObject> prefabInstances)
+        { foreach (GameObject instance in prefabInstances) BreakPrefabInstance(instance); }
+        /// <summary>
+        /// Completely removes any prefab connection from the given prefab instance.
+        /// Based on RodGreen's method (http://forum.unity3d.com/threads/82883-Breaking-connection-from-gameObject-to-prefab-for-good.?p=726602&amp;viewfull=1#post726602)
+        /// </summary>
+        public static void BreakPrefabInstance(GameObject prefabInstance)
+        {
+            string name = prefabInstance.name;
+            Transform transform = prefabInstance.transform;
+            Transform parent = transform.parent;
+            // Unparent the GO so that world transforms are preserved.
+            transform.parent = null;
+            // Clone and re-assign.
+            GameObject newInstance = (GameObject)Object.Instantiate(prefabInstance);
+            newInstance.name = name;
+            newInstance.SetActive(prefabInstance.activeSelf);
+            newInstance.transform.parent = parent;
+            // Remove old.
+            Object.DestroyImmediate(prefabInstance, false);
         }
     }
 }
