11b2e75dafd2c0dec247f8988be92ee3ee7a7f0d
Demigiant <daniele.giardini@gmail.com>
2016-08-16 14:47:57 +0200

    Core > Updated SerializedPropertyExtensions.CastTo to work also with list elements

---
+++ _Demigiant.Libraries/Core/DemiEditor/Extensions/SerializedPropertyExtensions.cs
@@ -0,0 +1,79 @@
+﻿// Author: Daniele Giardini - http://www.demigiant.com
+// Created: 2016/08/16 11:31
+// License Copyright (c) Daniele Giardini
+
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+namespace DG.DemiEditor
+{
+    public static class SerializedPropertyExtensions
+    {
+        #region Public Methods
+
+        /// <summary>
+        /// Returns the value of the given property (works like a cast to type).
+        /// <para>
+        /// Improved from HiddenMonk's functions (http://answers.unity3d.com/questions/627090/convert-serializedproperty-to-custom-class.html)
+        /// </para>
+        /// </summary>
+        public static T CastTo<T>(this SerializedProperty property)
+        {
+            object obj = property.serializedObject.targetObject;
+            string[] paths = property.propertyPath.Split('.');
+            foreach (string part in paths) {
+                if (part == "Array") {
+                    // Array or list
+                    int index = property.GetIndexInArray(); // Can be out of range in case the element had just been created
+                    IList<T> iList = (IList<T>)obj;
+                    return iList.Count - 1 < index ? default(T) : iList[index];
+                }
+                obj = GetFieldOrPropertyValue<object>(part, obj);
+            }
+            return (T)obj;
+        }
+
+        /// <summary>
+        /// Returns TRUE if this property is inside an array
+        /// </summary>
+        public static bool IsArrayElement(this SerializedProperty property)
+        {
+            return property.propertyPath.Contains("Array");
+        }
+
+        /// <summary>
+        /// Returns -1 if the property is not inside an array, otherwise returns its index inside the array
+        /// </summary>
+        public static int GetIndexInArray(this SerializedProperty property)
+        {
+            if (!property.IsArrayElement()) return -1;
+
+            int cutFrom = property.propertyPath.LastIndexOf('[') + 1;
+            int cutLen = property.propertyPath.LastIndexOf(']') - cutFrom;
+            return int.Parse(property.propertyPath.Substring(cutFrom, cutLen));
+        }
+
+        #endregion
+
+        #region Methods
+
+        static T GetFieldOrPropertyValue<T>(string fieldName, object obj)
+        {
+            const BindingFlags bindings = BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic;
+            Type t = obj.GetType();
+
+            FieldInfo field = t.GetField(fieldName, bindings);
+            if (field != null) return (T)field.GetValue(obj);
+ 
+            PropertyInfo property = t.GetProperty(fieldName, bindings);
+            if (property != null) return (T)property.GetValue(obj, null);
+ 
+            return default(T);
+        }
+
+        #endregion
+    }
+}
