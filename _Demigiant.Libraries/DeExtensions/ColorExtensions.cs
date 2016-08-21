e0d549706a328354a414f2799622911f9cc50eb5
Demigiant <daniele.giardini@gmail.com>
2016-08-21 12:38:03 +0200

    Renamed ColorExtension ColorToHex to ToHex

--- _Demigiant.Libraries/DeExtensions/ColorExtensions.cs
+++ _Demigiant.Libraries/DeExtensions/ColorExtensions.cs
@@ -17,6 +17,14 @@ namespace DG.DeExtensions
         }
 
         /// <summary>
+        /// Returns a HEX version of the given Unity Color, without the initial #
+        /// </summary>
+        public static string ToHex(this Color color)
+        {
+            return color.r.ToString("x2") + color.g.ToString("x2") + color.b.ToString("x2");
+        }
+
+        /// <summary>
         /// Changes the alpha of this color and returns it
         /// </summary>
         public static Color SetAlpha(this Color color, float alpha)
