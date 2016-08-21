f1d91ac61b1906fa3f63f456ffceb8f7569bfc42
Demigiant <daniele.giardini@gmail.com>
2016-08-21 12:11:05 +0200

    Added DeRectExtensions

--- _Demigiant.Libraries/DeExtensions/RectExtensions.cs
+++ _Demigiant.Libraries/DeExtensions/RectExtensions.cs
@@ -1,22 +1,17 @@
 ﻿// Author: Daniele Giardini - http://www.demigiant.com
-// Created: 2015/12/15 18:37
+// Created: 2016/08/21 11:57
 // License Copyright (c) Daniele Giardini
 
-using System;
 using UnityEngine;
 
-namespace DG.DeUtils
+namespace DG.DeExtensions
 {
-    /// <summary>
-    /// Utils for structs
-    /// </summary>
-    public static class DeRectUtils
+    public static class RectExtensions
     {
         /// <summary>
         /// Adds one rect into another, and returns the resulting a
         /// </summary>
-        [Obsolete("DeRectUtils.Add is deprecated, use the myRect.Add extension instead (in the DeExtensions library).")]
-        public static Rect Add(Rect a, Rect b)
+        public static Rect Add(this Rect a, Rect b)
         {
             if (b.xMin < a.xMin) a.xMin = b.xMin;
             if (b.xMax > a.xMax) a.xMax = b.xMax;
@@ -33,8 +28,7 @@ namespace DG.DeUtils
         /// <param name="h">Height to fit</param>
         /// <param name="shrinkOnly">If TRUE (default) only shrinks the rect if needed, if FALSE also enlarges it to fit</param>
         /// <returns></returns>
-        [Obsolete("DeRectUtils.Fit is deprecated, use the myRect.Fit extension instead (in the DeExtensions library).")]
-        public static Rect Fit(Rect r, float w, float h, bool shrinkOnly = true)
+        public static Rect Fit(this Rect r, float w, float h, bool shrinkOnly = true)
         {
             if (shrinkOnly && r.width <= w && r.height <= h) return r;
             float wPerc = w / r.width;
@@ -48,8 +42,7 @@ namespace DG.DeUtils
         /// <summary>
         /// Returns TRUE if the first rect fully includes the second one
         /// </summary>
-        [Obsolete("DeRectUtils.Includes is deprecated, use the myRect.Includes extension instead (in the DeExtensions library).")]
-        public static bool Includes(Rect a, Rect b)
+        public static bool Includes(this Rect a, Rect b)
         {
             return a.xMin <= b.xMin && a.xMax > b.xMax && a.yMin <= b.yMin && a.yMax >= b.yMax;
         }
@@ -57,8 +50,7 @@ namespace DG.DeUtils
         /// <summary>
         /// Modifies and returns the given Rect, with its values shifted according the the given parameters
         /// </summary>
-        [Obsolete("DeRectUtils.Shift is deprecated, use the myRect.Shift extension instead (in the DeExtensions library).")]
-        public static Rect Shift(Rect rect, float x, float y, float width, float height)
+        public static Rect Shift(this Rect rect, float x, float y, float width, float height)
         {
             return new Rect(rect.x + x, rect.y + y, rect.width + width, rect.height + height);
         }
