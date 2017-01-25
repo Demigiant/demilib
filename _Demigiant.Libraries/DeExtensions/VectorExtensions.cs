52774413978eea04ea8aee2b187518a1afafbf4f
Demigiant <daniele.giardini@gmail.com>
2017-01-25 20:59:03 +0100

    [DeExtensions] Added VectorExtensions.AngleY + [DeUtils] Removed DeVectorUtils

--- _Demigiant.Libraries/DeExtensions/VectorExtensions.cs
+++ _Demigiant.Libraries/DeExtensions/VectorExtensions.cs
@@ -1,17 +1,17 @@
 ﻿// Author: Daniele Giardini - http://www.demigiant.com
-// Created: 2017/01/25 20:48
+// Created: 2017/01/25 20:53
 // License Copyright (c) Daniele Giardini
 
 using UnityEngine;
 
-namespace DG.DeUtils
+namespace DG.DeExtensions
 {
-    public static class DeVectorUtils
+    public static class VectorExtensions
     {
         /// <summary>
         /// Returns the Y angle in degrees of the given vector, based on world coordinates.
         /// </summary>
-        public static float AngleY(Vector3 v)
+        public static float AngleY(this Vector3 v)
         {
             float angle = Vector3.Angle(Vector3.forward, v);
             if (v.x < 0) angle = 360 - angle;
