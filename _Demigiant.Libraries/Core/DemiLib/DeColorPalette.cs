ed597890ae26876556831f19f54a34121f067fa5
Demigiant <daniele.giardini@gmail.com>
2017-02-23 12:13:28 +0100

    [DeGUI] Nicer default colors for DeGUI/Layout Divider

--- _Demigiant.Libraries/Core/DemiLib/DeColorPalette.cs
+++ _Demigiant.Libraries/Core/DemiLib/DeColorPalette.cs
@@ -64,7 +64,7 @@ namespace DG.DemiLib
         public DeSkinColor def = Color.white;
 //        public DeSkinColor critical = Color.red;
         public DeSkinColor critical = new DeSkinColor(new Color(0.9411765f, 0.2388736f, 0.006920422f, 1f), new Color(1f, 0.2482758f, 0f, 1f));
-        public DeSkinColor divider = new DeSkinColor(new Color(0.5f, 0.5f, 0.5f, 1), Color.black);
+        public DeSkinColor divider = new DeSkinColor(0.6f, 0.3f);
         public DeSkinColor toggleOn = new DeSkinColor(new Color(0.3158468f, 0.875f, 0.1351103f, 1f), new Color(0.2183823f, 0.7279412f, 0.09099264f, 1f));
         public DeSkinColor toggleOff = new DeSkinColor(0.75f, 0.4f);
     }
