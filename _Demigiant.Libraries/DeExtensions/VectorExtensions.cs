7e2b320dff4d9eb118db5861bc2a076672909629
Demigiant <daniele.giardini@gmail.com>
2017-06-12 16:06:13 +0200

    [DeExtensions] Added Vector3.SetX/Y/Z methods + [DeUtils] Added DeMathUtils.FindLineCircleIntersection + DeMathUtils.FindNearestLineCircleIntersection methods

--- _Demigiant.Libraries/DeExtensions/VectorExtensions.cs
+++ _Demigiant.Libraries/DeExtensions/VectorExtensions.cs
@@ -12,19 +12,65 @@ namespace DG.DeExtensions
         #region Vector2
 
         /// <summary>
+        /// Returns a copy of the vector with its X set to the given value.
+        /// </summary>
+        public static Vector2 SetX(this Vector2 v, float value)
+        {
+            v.x = value;
+            return v;
+        }
+        /// <summary>
+        /// Returns a copy of the vector with its Y set to the given value.
+        /// </summary>
+        public static Vector2 SetY(this Vector2 v, float value)
+        {
+            v.y = value;
+            return v;
+        }
+
+        /// <summary>
         /// Returns a copy of the vector with its values rounded to integers, using Mathf.Round.
         /// </summary>
-        public static Vector2 Round(this Vector2 value)
+        public static Vector2 Round(this Vector2 v)
         {
-            return new Vector2(Mathf.Round(value.x), Mathf.Round(value.y));
+            return new Vector2(Mathf.Round(v.x), Mathf.Round(v.y));
         }
 
         /// <summary>
         /// Returns a copy of the vector with its values rounded to integers, using a fast int cast.
         /// </summary>
-        public static Vector2 Floor(this Vector2 value)
+        public static Vector2 Floor(this Vector2 v)
         {
-            return new Vector2((int)value.x, (int)value.y);
+            return new Vector2((int)v.x, (int)v.y);
+        }
+
+        #endregion
+
+        #region Vector3
+
+        /// <summary>
+        /// Returns a copy of the vector with its X set to the given value.
+        /// </summary>
+        public static Vector3 SetX(this Vector3 v, float value)
+        {
+            v.x = value;
+            return v;
+        }
+        /// <summary>
+        /// Returns a copy of the vector with its Y set to the given value.
+        /// </summary>
+        public static Vector3 SetY(this Vector3 v, float value)
+        {
+            v.y = value;
+            return v;
+        }
+        /// <summary>
+        /// Returns a copy of the vector with its Z set to the given value.
+        /// </summary>
+        public static Vector3 SetZ(this Vector3 v, float value)
+        {
+            v.z = value;
+            return v;
         }
 
         #endregion
--- _Demigiant.Libraries/DeUtils/DeMathUtils.cs
+++ _Demigiant.Libraries/DeUtils/DeMathUtils.cs
@@ -8,6 +8,8 @@ namespace DG.DeUtils
 {
     public static class DeMathUtils
     {
+        #region Public Methods
+
         /// <summary>
         /// Clamps an angle between the given values.
         /// </summary>
@@ -47,5 +49,62 @@ namespace DG.DeUtils
             if (!result) angle = max;
             return angle;
         }
+
+        /// <summary>
+        /// Finds the eventual intersection points between a line and a circle, and returns the total number of intersections
+        /// </summary>
+        /// Uses code from Rod Stephens: http://csharphelper.com/blog/2014/09/determine-where-a-line-intersects-a-circle-in-c/
+        public static int FindLineCircleIntersection(Vector2 circleCenter, float radius, Vector2 lineStartP, Vector2 lineEndP, out Vector2 intersection0, out Vector2 intersection1)
+        {
+            float dx = lineEndP.x - lineStartP.x;
+            float dy = lineEndP.y - lineStartP.y;
+
+            float a = dx * dx + dy * dy;
+            float b = 2 * (dx * (lineStartP.x - circleCenter.x) + dy * (lineStartP.y - circleCenter.y));
+            float c = (lineStartP.x - circleCenter.x) * (lineStartP.x - circleCenter.x) +
+                      (lineStartP.y - circleCenter.y) * (lineStartP.y - circleCenter.y) -
+                      radius * radius;
+
+            float det = b * b - 4 * a * c;
+            if (a <= 0.0000001f || det < 0) {
+                // No real solutions.
+                intersection0 = new Vector2(float.NaN, float.NaN);
+                intersection1 = new Vector2(float.NaN, float.NaN);
+                return 0;
+            } else if (det > 0) {
+                // Two solutions.
+                float t = (-b + Mathf.Sqrt(det)) / (2 * a);
+                intersection0 = new Vector2(lineStartP.x + t * dx, lineStartP.y + t * dy);
+                t = (-b - Mathf.Sqrt(det)) / (2 * a);
+                intersection1 = new Vector2(lineStartP.x + t * dx, lineStartP.y + t * dy);
+                return 2;
+            } else {
+                // One solution.
+                float t = -b / (2 * a);
+                intersection0 = new Vector2(lineStartP.x + t * dx, lineStartP.y + t * dy);
+                intersection1 = new Vector2(float.NaN, float.NaN);
+                return 1;
+            }
+        }
+
+        /// <summary>
+        /// Finds the closest intersection point between a line and a circle, and returns TRUE if it's found, FALSE otherwise
+        /// </summary>
+        public static bool FindNearestLineCircleIntersection(Vector2 circleCenter, float radius, Vector2 lineStartP, Vector2 lineEndP, out Vector2 nearestIntersection)
+        {
+            Vector2 p0, p1;
+            int res = FindLineCircleIntersection(circleCenter, radius, lineStartP, lineEndP, out p0, out p1);
+            if (res == 0) {
+                nearestIntersection = new Vector2(float.NaN, float.NaN);
+                return false;
+            } else {
+                if (Mathf.Abs(p0.x - lineStartP.x) < Mathf.Abs(p1.x - lineStartP.x) || Mathf.Abs(p0.y - lineStartP.y) < Mathf.Abs(p1.y - lineStartP.y)) {
+                    nearestIntersection = p0;
+                } else nearestIntersection = p1;
+                return true;
+            }
+        }
+
+        #endregion
     }
 }
