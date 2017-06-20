9cf676587e03dd27721b8fc1cf53b225736dfba4
Demigiant <daniele.giardini@gmail.com>
2017-06-20 14:39:01 +0200

    [De2D] Added DeUIMesh component (used to draw meshes inside Unity's UI)

---
+++ _Demigiant.Libraries/De2D/UI/DeUIMesh.cs
@@ -0,0 +1,61 @@
+﻿// Author: Daniele Giardini - http://www.demigiant.com
+// Created: 2017/06/20 13:59
+// License Copyright (c) Daniele Giardini
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DG.De2D.UI
+{
+    /// <summary>
+    /// Used to draw a custom mesh inside Unity's UI
+    /// </summary>
+    public class DeUIMesh : MaskableGraphic
+    {
+        #region Serialized
+
+    [Header("Mesh Data")]
+        [SerializeField] Vector2[] _vertices;
+        [SerializeField] Vector2[] _uvs;
+        [SerializeField] int[] _trisIndexes;
+        
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Draws a mesh with the given data. Coordinates are Canvas-based.
+        /// </summary>
+        public void Draw(Vector2[] vertices, int[] trisIndexes, Vector2[] uvs, Color color)
+        {
+            _vertices = vertices;
+            _trisIndexes = trisIndexes;
+            _uvs = uvs;
+            this.color = color;
+            SetAllDirty();
+        }
+
+        #endregion
+
+        #region Methods
+
+
+
+        protected override void OnPopulateMesh(VertexHelper vHelper)
+        {
+            if (_vertices == null || _uvs == null || _trisIndexes == null) return;
+            int verticesLen = _vertices.Length;
+            int uvsLen = _uvs.Length;
+            int trisIndexesLen = _trisIndexes.Length;
+            if (verticesLen != uvsLen) return;
+            if (trisIndexesLen % 3 != 0) return;
+
+			vHelper.Clear();
+
+            for (int i = 0; i < verticesLen; ++i) vHelper.AddVert(_vertices[i], color, _uvs[i]);
+            for (int i = 0; i < trisIndexesLen; i += 3) vHelper.AddTriangle(_trisIndexes[i], _trisIndexes[i+1], _trisIndexes[i+2]);
+		}
+
+        #endregion
+    }
+}
---
+++ _Demigiant.Libraries/De2DEditor/MenuItems.cs
@@ -0,0 +1,28 @@
+﻿// Author: Daniele Giardini - http://www.demigiant.com
+// Created: 2017/06/20 14:00
+// License Copyright (c) Daniele Giardini
+
+using DG.De2D.UI;
+using UnityEditor;
+using UnityEngine;
+
+namespace DG.De2DEditor
+{
+    public static class MenuItems
+    {
+        [MenuItem("GameObject/UI/DeUIMesh", false)]
+        static void CreateUIMesh(MenuCommand menuCommand)
+        {
+            GameObject contextGO = menuCommand.context as GameObject;
+            if (contextGO == null || contextGO.GetComponentInParent<Canvas>() == null) {
+                EditorUtility.DisplayDialog("Add DeUIMesh", "DeUIMesh can only be added inside a UI Canvas", "Ok");
+                return;
+            }
+            GameObject go = new GameObject("DeUIMesh");
+            go.AddComponent<DeUIMesh>();
+            GameObjectUtility.SetParentAndAlign(go, contextGO);
+            Undo.RegisterCreatedObjectUndo(go, "Create " + go.name);
+            Selection.activeObject = go;
+        }
+    }
+}
