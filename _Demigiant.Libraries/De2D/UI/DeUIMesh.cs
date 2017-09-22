5c4c820ac40c48aefde3b8b9fe1acd1bd891f1db
Demigiant <daniele.giardini@gmail.com>
2017-09-22 12:58:46 +0200

    [De2D] Added multiple UVs support to DeUIMesh

--- _Demigiant.Libraries/De2D/UI/DeUIMesh.cs
+++ _Demigiant.Libraries/De2D/UI/DeUIMesh.cs
@@ -16,10 +16,14 @@ namespace DG.De2D.UI
 
     [Header("Mesh Data")]
         [SerializeField] Vector2[] _vertices;
-        [SerializeField] Vector2[] _uvs;
+        [SerializeField] Vector2[] _uv0;
+        [SerializeField] Vector2[] _uv1;
         [SerializeField] int[] _trisIndexes;
         
         #endregion
+
+        static Vector3 _defNormal = Vector3.back;
+        static Vector4 _defTangent = new Vector4(1f, 0f, 0f, -1f);
 
         CanvasRenderer _cRenderer {
             get {
@@ -35,11 +39,17 @@ namespace DG.De2D.UI
         /// Draws a mesh with the given data. Coordinates are Canvas-based.
         /// </summary>
         public void Draw(Vector2[] vertices, int[] trisIndexes, Vector2[] uvs, Color color)
+        { Draw(vertices, trisIndexes, uvs, null, color); }
+        /// <summary>
+        /// Draws a mesh with the given data. Coordinates are Canvas-based.
+        /// </summary>
+        public void Draw(Vector2[] vertices, int[] trisIndexes, Vector2[] uv0, Vector2[] uv1, Color color)
         {
             _cRenderer.cull = false;
             _vertices = vertices;
             _trisIndexes = trisIndexes;
-            _uvs = uvs;
+            _uv0 = uv0;
+            _uv1 = uv1;
             this.color = color;
             SetAllDirty();
         }
@@ -60,16 +70,19 @@ namespace DG.De2D.UI
 
         protected override void OnPopulateMesh(VertexHelper vHelper)
         {
-            if (_vertices == null || _uvs == null || _trisIndexes == null) return;
+            if (_vertices == null || _uv0 == null || _trisIndexes == null) return;
             int verticesLen = _vertices.Length;
-            int uvsLen = _uvs.Length;
+            int uvsLen = _uv0.Length;
             int trisIndexesLen = _trisIndexes.Length;
             if (verticesLen != uvsLen) return;
             if (trisIndexesLen % 3 != 0) return;
 
 			vHelper.Clear();
 
-            for (int i = 0; i < verticesLen; ++i) vHelper.AddVert(_vertices[i], color, _uvs[i]);
+            for (int i = 0; i < verticesLen; ++i) {
+                if (_uv1 != null) vHelper.AddVert(_vertices[i], color, _uv0[i], _uv1[i], _defNormal, _defTangent);
+                else vHelper.AddVert(_vertices[i], color, _uv0[i]);
+            }
             for (int i = 0; i < trisIndexesLen; i += 3) vHelper.AddTriangle(_trisIndexes[i], _trisIndexes[i+1], _trisIndexes[i+2]);
 		}
 
