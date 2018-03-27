26af9538d888bfac01b4f1b7a5aff60a93ce38c3
Demigiant <daniele.giardini@gmail.com>
2018-03-27 13:38:34 +0200

    [DeAudio] Addendum for previous update: fixed bug when using FadeTo on an AudioSource whose group has lower than 1 volume

--- _Demigiant.Libraries/DeAudio/DeAudioManager.cs
+++ _Demigiant.Libraries/DeAudio/DeAudioManager.cs
@@ -31,7 +31,7 @@ namespace DG.DeAudio
         }
 
         internal static DeAudioManager I;
-        public const string Version = "0.6.160";
+        public const string Version = "0.6.165";
         internal const string LogPrefix = "DeAudio :: ";
         static bool _isInitializing; // If TRUE skips audioGroups initialization at Awake
         internal static DeAudioGroup[] audioGroups; // Internal so Inspector can read it
