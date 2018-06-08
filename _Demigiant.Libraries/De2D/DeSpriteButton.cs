88158dd2016be2ce4123a62fce9ad8bccd8785e3
Demigiant <daniele.giardini@gmail.com>
2018-06-08 15:42:26 +0200

    [De2D] Added DeSpriteButton

---
+++ _Demigiant.Libraries/De2D/DeSpriteButton.cs
@@ -0,0 +1,184 @@
+﻿// Author: Daniele Giardini - http://www.demigiant.com
+// Created: 2018/06/07 21:49
+// License Copyright (c) Daniele Giardini
+
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace DG.De2D
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class DeSpriteButton : MonoBehaviour
+    {
+        enum State
+        {
+            Normal,
+            Rollover,
+            Press
+        }
+
+        #region EVENTS
+
+        public event Action OnEditorRefreshRequired;
+        void Dispatch_OnEditorRefreshRequired() { if (OnEditorRefreshRequired != null) OnEditorRefreshRequired(); }
+
+        #endregion
+
+        #region Serialized
+
+        [SerializeField] bool _interactable = true;
+        public ColorBlock colors = ColorBlock.defaultColorBlock;
+        public UnityEvent onClick;
+
+        #endregion
+
+        public bool interactable { get { return _interactable; } set { SetInteractable(value); } }
+
+        bool _initialized;
+        State _state = State.Normal;
+        SpriteRenderer _spriteR;
+        bool _isOver;
+        bool _isDown;
+        Coroutine _coColorTween;
+
+        #region Unity + INIT
+
+        void Init()
+        {
+            if (_initialized) return;
+
+            _initialized = true;
+
+            _spriteR = this.GetComponent<SpriteRenderer>();
+        }
+
+        void Awake()
+        {
+            Init();
+        }
+
+        void Reset()
+        {
+            Dispatch_OnEditorRefreshRequired();
+        }
+
+        void OnMouseEnter()
+        {
+            _state = _isDown ? State.Press : State.Rollover;
+            _isOver = true;
+            Refresh();
+        }
+
+        void OnMouseDown()
+        {
+            _state = State.Press;
+            _isDown = true;
+            Refresh();
+        }
+
+        void OnMouseUpAsButton()
+        {
+            _state = State.Rollover;
+            _isDown = false;
+            Refresh();
+            if (onClick != null) onClick.Invoke();
+        }
+
+        void OnMouseUp()
+        {
+            _state = _isOver ? State.Rollover : State.Normal;
+            _isDown = false;
+            Refresh();
+        }
+
+        void OnMouseExit()
+        {
+            _state = _isDown ? State.Press : State.Normal;
+            _isOver = false;
+            Refresh();
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Force-refreshes the graphics of this button
+        /// </summary>
+        public void Refresh()
+        {
+            Init();
+
+            if (!_interactable) {
+                TweenColorTo(colors.disabledColor, colors.fadeDuration);
+            } else {
+                switch (_state) {
+                case State.Rollover:
+                    TweenColorTo(colors.highlightedColor, colors.fadeDuration);
+                    break;
+                case State.Press:
+                    TweenColorTo(colors.pressedColor, -1);
+                    break;
+                default:
+                    TweenColorTo(colors.normalColor, colors.fadeDuration);
+                    break;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        void SetInteractable(bool value)
+        {
+            if (_interactable == value) return;
+
+            _interactable = value;
+            Refresh();
+        }
+
+        #region Tweens
+
+        void TweenColorTo(Color color, float duration)
+        {
+            if (_coColorTween != null) {
+                this.StopCoroutine(_coColorTween);
+                _coColorTween = null;
+            }
+            if (duration <= 0) {
+                _spriteR.color = color;
+                return;
+            }
+            if (_spriteR.color.Equals(color)) return;
+
+            _coColorTween = this.StartCoroutine(CO_ColorTo(color, duration));
+        }
+
+        IEnumerator CO_ColorTo(Color color, float duration)
+        {
+            if (duration <= 0) {
+                _spriteR.color = color;
+                yield break;
+            }
+            Color startColor = _spriteR.color;
+            float startTime = Time.realtimeSinceStartup;
+            bool complete = false;
+            while (!complete) {
+                float elapsed = Time.realtimeSinceStartup - startTime;
+                float elapsedPerc = Mathf.Min(1, elapsed / duration);
+                _spriteR.color = Color.Lerp(startColor, color, elapsedPerc);
+                if (elapsed > duration) complete = true;
+                else yield return null;
+            }
+            _coColorTween = null;
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
---
+++ _Demigiant.Libraries/De2DEditor/DeSpriteButtonInspector.cs
@@ -0,0 +1,131 @@
+﻿// Author: Daniele Giardini - http://www.demigiant.com
+// Created: 2018/06/07 21:51
+// License Copyright (c) Daniele Giardini
+
+using DG.De2D;
+using DG.DemiEditor;
+using UnityEditor;
+using UnityEngine;
+
+namespace DG.De2DEditor
+{
+    [CustomEditor(typeof(DeSpriteButton)), CanEditMultipleObjects]
+    public class DeSpriteButtonInspector : Editor
+    {
+        enum ColliderType
+        {
+            Box,
+            Circle,
+            Capsule,
+            Polygon
+        }
+
+        DeSpriteButton _src;
+
+        SerializedProperty _p_interactable,
+                           _p_colors,
+                           _p_onClick;
+
+        #region Unity and GUI Methods
+
+        void OnEnable()
+        {
+            _src = this.target as DeSpriteButton;
+
+            _p_interactable = serializedObject.FindProperty("_interactable");
+            _p_colors = serializedObject.FindProperty("colors");
+            _p_onClick = serializedObject.FindProperty("onClick");
+
+            _src.OnEditorRefreshRequired += RefreshSprite;
+        }
+
+        void OnDisable()
+        {
+            _src.OnEditorRefreshRequired -= RefreshSprite;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            DeGUI.BeginGUI();
+            bool missingCollider = false;
+            foreach (Object obj in serializedObject.targetObjects) {
+                if (((DeSpriteButton)obj).GetComponent<Collider2D>() == null) {
+                    missingCollider = true;
+                    break;
+                }
+            }
+            if (missingCollider) {
+                EditorGUILayout.HelpBox("You need to attach a Collider2D to this object", MessageType.Warning);
+                using (new DeGUI.ColorScope(null, null, Color.yellow))
+                using (new GUILayout.VerticalScope(DeGUI.styles.box.def)) {
+                    if (GUILayout.Button("Attach BoxCollider2D")) AttachCollider(ColliderType.Box);
+                    if (GUILayout.Button("Attach CircleCollider2D")) AttachCollider(ColliderType.Circle);
+                    if (GUILayout.Button("Attach CapsuleCollider2D")) AttachCollider(ColliderType.Capsule);
+                    if (GUILayout.Button("Attach PolygonCollider2D")) AttachCollider(ColliderType.Polygon);
+                }
+            } else {
+                const string message = "You need to order your sprites via z-depth, not \"Order in Layer\"." +
+                                       "\nAlso note that Trigger colliders will work only if your Physics2D settings allow raycasting on Triggers.";
+                EditorGUILayout.HelpBox(message, MessageType.Info);
+            }
+
+            bool refreshSprite = false;
+
+            serializedObject.Update();
+
+            using (var check = new EditorGUI.ChangeCheckScope()) {
+                EditorGUILayout.PropertyField(_p_interactable);
+                EditorGUI.indentLevel++;
+                EditorGUILayout.PropertyField(_p_colors);
+                EditorGUI.indentLevel--;
+                if (check.changed) refreshSprite = true;
+            }
+            GUILayout.Space(4);
+            EditorGUILayout.PropertyField(_p_onClick);
+
+            serializedObject.ApplyModifiedProperties();
+
+            if (refreshSprite) RefreshSprite();
+        }
+
+        #endregion
+
+        #region Methods
+
+        void RefreshSprite()
+        {
+            foreach (Object obj in serializedObject.targetObjects) {
+                DeSpriteButton bt = (DeSpriteButton)obj;
+                SpriteRenderer spriteR = bt.GetComponent<SpriteRenderer>();
+                Undo.RecordObject(spriteR, "DeSpriteButton");
+                spriteR.color = (bt.interactable ? bt.colors.normalColor : bt.colors.disabledColor) * bt.colors.colorMultiplier;
+                EditorUtility.SetDirty(spriteR);
+            }
+        }
+
+        void AttachCollider(ColliderType type)
+        {
+            foreach (Object obj in serializedObject.targetObjects) {
+                DeSpriteButton bt = (DeSpriteButton)obj;
+                Collider2D coll = bt.GetComponent<Collider2D>();
+                if (coll != null) continue;
+                switch (type) {
+                case ColliderType.Box:
+                    Undo.AddComponent<BoxCollider2D>(bt.gameObject);
+                    break;
+                case ColliderType.Circle:
+                    Undo.AddComponent<CircleCollider2D>(bt.gameObject);
+                    break;
+                case ColliderType.Capsule:
+                    Undo.AddComponent<CapsuleCollider2D>(bt.gameObject);
+                    break;
+                case ColliderType.Polygon:
+                    Undo.AddComponent<PolygonCollider2D>(bt.gameObject);
+                    break;
+                }
+            }
+        }
+
+        #endregion
+    }
+}
