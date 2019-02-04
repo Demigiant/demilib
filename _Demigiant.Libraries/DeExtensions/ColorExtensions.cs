e1dbf4aa7d7bab57da10889b5125df09b615d80b
Demigiant <daniele.giardini@gmail.com>
2019-02-04 13:14:24 +0100

    [DeExtensions] Added Color.ChangeBrightness

--- _Demigiant.Libraries/DeExtensions/ColorExtensions.cs
+++ _Demigiant.Libraries/DeExtensions/ColorExtensions.cs
@@ -9,6 +9,34 @@ namespace DG.DeExtensions
     public static class ColorExtensions
     {
         /// <summary>
+        /// Returns a new color equal to the given one with changed brightness
+        /// </summary>
+        /// <param name="color">Color to evaluate</param>
+        /// <param name="brightnessFactor">Brightness factor (multiplied by current brightness)</param>
+        /// <param name="alpha">If set applies this alpha value</param>
+        /// <returns></returns>
+        public static Color ChangeBrightness(this Color color, float brightnessFactor, float? alpha = null)
+        {
+            float h, s, v;
+            Color.RGBToHSV(color, out h, out s, out v);
+            v *= brightnessFactor;
+            if (v < 0) v = 0;
+            else if (v > 1) v = 1;
+            Color result = Color.HSVToRGB(h, s, v);
+            if (alpha != null) result.a = (float)alpha;
+            return result;
+        }
+
+        /// <summary>
+        /// Changes the alpha of this color and returns it
+        /// </summary>
+        public static Color SetAlpha(this Color color, float alpha)
+        {
+            color.a = alpha;
+            return color;
+        }
+
+        /// <summary>
         /// Returns a HEX version of the given Unity Color, without the initial #
         /// </summary>
         /// <param name="includeAlpha">If TRUE, also converts the alpha value and returns a hex of 8 characters,
@@ -28,15 +56,6 @@ namespace DG.DeExtensions
         public static string ToHex(this Color color, bool includeAlpha = false)
         {
             return ToHex((Color32)color, includeAlpha);
-        }
-
-        /// <summary>
-        /// Changes the alpha of this color and returns it
-        /// </summary>
-        public static Color SetAlpha(this Color color, float alpha)
-        {
-            color.a = alpha;
-            return color;
         }
     }
 }
