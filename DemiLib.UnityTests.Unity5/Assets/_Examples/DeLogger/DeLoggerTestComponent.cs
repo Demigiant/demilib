aa9a63bad8d142653055ce7ba74677389095f6a9
Demigiant <daniele.giardini@gmail.com>
2019-02-10 11:32:22 +0100

    [Debugging] Added DeloggerExtensions with list.ToLogString methods

---
+++ DemiLib.UnityTests.Unity5/Assets/_Examples/DeLogger/DeLoggerTestComponent.cs
@@ -0,0 +1,86 @@
+﻿// Author: Daniele Giardini - http://www.demigiant.com
+// Created: 2019/02/10 10:23
+// License Copyright (c) Daniele Giardini
+
+using System.Collections.Generic;
+using DG.Debugging;
+using DG.DeInspektor.Attributes;
+using UnityEngine;
+
+namespace _Examples.DeLogger
+{
+    public class DeLoggerTestComponent : MonoBehaviour
+    {
+        #region Serialized
+
+        [DeToggleButton]
+        [SerializeField] bool _stripHtmlTags = false;
+
+        #endregion
+
+        readonly List<int> _intList = new List<int>() {6, 8, 10};
+        readonly Vector2[] _vectorsList = new[] {new Vector2(4, 5), new Vector2(8,9)};
+        readonly SampleObject[] _sampleObjArray = new [] {
+            new SampleObject("Luna", 678), // properties are: id, code
+            new SampleObject("Mars", 5489), // properties are: id, code
+            new SampleObject("Phobos", 21), // properties are: id, code
+        };
+
+        void Examples()
+        {
+            Debug.Log(_intList.ToLogString());
+            // ▼ Logs ▼
+            // 6 - 8 - 10
+            Debug.Log("_vectorsList: " + _vectorsList.ToLogString());
+            // ▼ Logs ▼
+            // _vectorsList: (4.0, 5.0) - (8.0, 9.0)
+            Debug.Log("Oxygen tanks values:" + _intList.ToLogString(true));
+            // ▼ Logs ▼
+            // Oxygen tanks values:
+            // 0: 6
+            // 1: 8
+            // 2: 10
+            Debug.Log("Something something IDs:" + _sampleObjArray.ToLogString(x => x.id, true));
+            // ▼ Logs ▼
+            // Something something IDs:
+            // 0: Luna
+            // 1: Mars
+            // 2: Phobos
+        }
+
+        #region Test Methods
+
+        [DeMethodButton("Log Lists/Arrays", 0, false)]
+        [DeMethodButton("Log Lists/Arrays w lineBreak", 0, true)]
+        void LogLists(bool lineBreak)
+        {
+            Debug.Log("An int list: " + _intList.ToLogString(lineBreak));
+            Debug.Log("A Vector2 list: " + _vectorsList.ToLogString(lineBreak));
+            Debug.Log("A sample object array: " + _sampleObjArray.ToLogString(lineBreak));
+        }
+
+        [DeMethodButton("Log Lists/Arrays (ids only)", 1, false)]
+        [DeMethodButton("Log Lists/Arrays (ids only) w linebreak", 1, true)]
+        void LogListsWFormatting(bool lineBreak)
+        {
+            Debug.Log("A sample object array (string id only): " + _sampleObjArray.ToLogString(x => "id: " + x.id, lineBreak));
+        }
+
+        #endregion
+
+        // █████████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+        // ███ INTERNAL CLASSES ████████████████████████████████████████████████████████████████████████████████████████████████
+        // █████████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+
+        class SampleObject
+        {
+            public string id;
+            public int code;
+            public SampleObject(string id, int code)
+            {
+                this.id = id;
+                this.code = code;
+            }
+        }
+    }
+}
---
+++ _Demigiant.Libraries/Debugging/DeloggerExtensions.cs
@@ -0,0 +1,59 @@
+﻿// Author: Daniele Giardini - http://www.demigiant.com
+// Created: 2019/02/10 09:58
+// License Copyright (c) Daniele Giardini
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DG.Debugging
+{
+    /// <summary>
+    /// Extensions used to format objects for logging
+    /// </summary>
+    public static class DeloggerExtensions
+    {
+        static readonly StringBuilder _Strb = new StringBuilder();
+
+        #region Public Methods
+
+        /// <summary>
+        /// Converts the given list into a log-formatted string
+        /// </summary>
+        /// <param name="list">List to log</param>
+        /// <param name="lineBreakWIndex">If TRUE starts each element with a lineBreak and prefixes it with its index</param>
+        public static string ToLogString<T>(this IList<T> list, bool lineBreakWIndex = false)
+        { return ToLogString(list, null, lineBreakWIndex); }
+
+        /// <summary>
+        /// Converts the given list into a log-formatted string
+        /// </summary>
+        /// <param name="list">List to log</param>
+        /// <param name="elaborate">Customized formatting function.
+        /// <para><code>Example:</code></para>
+        /// <para>Assuming a list of 4 gameObjects named "Gino", "Pino", "Lino", "Mandarino":</para>
+        /// <para><code>Debug.Log("A sample GameObject list:" + _sampleObjList.ToLogString(x => "My name is " + x.name, true));</code></para>
+        /// <para>Outputs:</para>
+        /// <code><para>A sample GameObject list:</para>
+        /// <para>0: My name is Gino</para>
+        /// <para>1: My name is Pino</para>
+        /// <para>2: My name is Lino</para>
+        /// <para>3: My name is Mandarino</para></code>
+        /// </param>
+        /// <param name="lineBreakWIndex">If TRUE starts each element with a lineBreak and prefixes it with its index</param>
+        public static string ToLogString<T>(this IList<T> list, Func<T,string> elaborate, bool lineBreakWIndex = false)
+        {
+            for (int i = 0; i < list.Count; ++i) {
+                if (lineBreakWIndex) _Strb.Append('\n').Append(i).Append(": ");
+                else if (i > 0) _Strb.Append(" - ");
+                if (list[i] == null) _Strb.Append(elaborate == null ? "NULL" : "[null list element]");
+                else _Strb.Append(elaborate == null ? list[i].ToString() : elaborate(list[i]));
+            }
+            string result = _Strb.ToString();
+            _Strb.Length = 0;
+            return result;
+        }
+
+        #endregion
+    }
+}
