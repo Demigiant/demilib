1bdf2ac95b9736fa7b59b534fdb9bd0b1248f032
Demigiant <daniele.giardini@gmail.com>
2019-02-10 21:20:33 +0100

    - minor

--- _Demigiant.Libraries/Core/DemiLib/DeColorPalette.cs
+++ _Demigiant.Libraries/Core/DemiLib/DeColorPalette.cs
@@ -85,6 +85,7 @@ namespace DG.DemiLib
         public DeSkinColor divider = new DeSkinColor(0.6f, 0.3f);
         public DeSkinColor toggleOn = new DeSkinColor(new Color(0.3158468f, 0.875f, 0.1351103f, 1f), new Color(0.2183823f, 0.7279412f, 0.09099264f, 1f));
         public DeSkinColor toggleOff = new DeSkinColor(0.75f, 0.4f);
+        public DeSkinColor toggleCritical = new DeSkinColor(new Color(0.9411765f, 0.2388736f, 0.006920422f, 1f), new Color(1f, 0.2482758f, 0f, 1f));
     }
 
     /// <summary>
@@ -97,5 +98,6 @@ namespace DG.DemiLib
         public DeSkinColor critical = new DeSkinColor(new Color(1f, 0.9148073f, 0.5588235f, 1f), new Color(1f, 0.3881846f, 0.3014706f, 1f));
         public DeSkinColor toggleOn = new DeSkinColor(new Color(1f, 0.9686275f, 0.6980392f, 1f), new Color(0.8025267f, 1f, 0.4705882f, 1f));
         public DeSkinColor toggleOff = new DeSkinColor(new Color(0.4117647f, 0.3360727f, 0.3360727f, 1f), new Color(0.6470588f, 0.5185986f, 0.5185986f, 1f));
+        public DeSkinColor toggleCritical = new DeSkinColor(new Color(1f, 0.84f, 0.62f), new Color(1f, 0.84f, 0.62f));
     }
 }
