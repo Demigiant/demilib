c3149326e8c6bbea1e1038df0efae11c61701861
Daniele Giardini <daniele.giardini@gmail.com>
2020-02-20 13:26:15 +0100

    [Core] Added more String extension methods

--- _Demigiant.Libraries/Core/DemiEditor/Extensions/StringExtensions.cs
+++ _Demigiant.Libraries/Core/DemiEditor/Extensions/StringExtensions.cs
@@ -5,6 +5,7 @@
 using System;
 using System.Globalization;
 using System.Reflection;
+using System.Text;
 using UnityEngine;
 using Object = UnityEngine.Object;
 
@@ -15,6 +16,85 @@ namespace DG.DemiEditor
     /// </summary>
     public static class StringExtensions
     {
+        static readonly StringBuilder _Strb = new StringBuilder();
+
+        #region Public Methods
+
+        /// <summary>
+        /// Returns TRUE if the string is null or empty
+        /// </summary>
+        /// <param name="trimSpaces">If TRUE (default) and the string contains only spaces, considers it empty</param>
+        public static bool IsNullOrEmpty(this string s, bool trimSpaces = true)
+        {
+            if (s == null) return true;
+            return trimSpaces ? s.Trim().Length == 0 : s.Length == 0;
+        }
+
+        /// <summary>
+        /// Compares a version string (in format #.#.###) with another of the same format,
+        /// and return TRUE if this one is minor. Boths trings must have the same number of dot separators.
+        /// </summary>
+        public static bool VersionIsMinorThan(this string s, string version)
+        {
+            string[] thisV = s.Split('.');
+            string[] otherV = version.Split('.');
+            if (thisV.Length != otherV.Length) throw new ArgumentException("Invalid");
+            for (int i = 0; i < thisV.Length; ++i) {
+                int thisInt = Convert.ToInt32(thisV[i]);
+                int otherInt = Convert.ToInt32(otherV[i]);
+                if (i == thisV.Length - 1) return thisInt < otherInt;
+                else if (thisInt == otherInt) continue;
+                else if (thisInt < otherInt) return true;
+                else if (thisInt > otherInt) return false;
+            }
+            throw new ArgumentException("Invalid");
+        }
+
+        /// <summary>
+        /// Converts a HEX color to a Unity Color and returns it
+        /// </summary>
+        /// <param name="hex">The HEX color, either with or without the initial # (accepts both regular and short format)</param>
+        public static Color HexToColor(this string hex)
+        {
+            if (hex[0] == '#') hex = hex.Substring(1);
+            int len = hex.Length;
+            bool isShortFormat = len < 6;
+            if (isShortFormat) {
+                float r = (HexToInt(hex[0]) + HexToInt(hex[0]) * 16f) / 255f;
+                float g = (HexToInt(hex[1]) + HexToInt(hex[1]) * 16f) / 255f;
+                float b = (HexToInt(hex[2]) + HexToInt(hex[2]) * 16f) / 255f;
+                float a = len == 4 ? (HexToInt(hex[3]) + HexToInt(hex[3]) * 16f) / 255f : 1;
+                return new Color(r, g, b, a);
+            } else {
+                float r = (HexToInt(hex[1]) + HexToInt(hex[0]) * 16f) / 255f;
+                float g = (HexToInt(hex[3]) + HexToInt(hex[2]) * 16f) / 255f;
+                float b = (HexToInt(hex[5]) + HexToInt(hex[4]) * 16f) / 255f;
+                float a = len == 8 ? (HexToInt(hex[7]) + HexToInt(hex[6]) * 16f) / 255f : 1;
+                return new Color(r, g, b, a);
+            }
+        }
+
+        /// <summary>
+        /// Nicifies a string, replacing underscores with spaces, and adding a space before Uppercase letters (except the first character)
+        /// </summary>
+        public static string Nicify(this string s)
+        {
+            if (string.IsNullOrEmpty(s)) return "";
+            _Strb.Length = 0;
+            _Strb.Append(s[0].ToString().ToUpper());
+            for (int i = 1; i < s.Length; i++) {
+                char curr = s[i];
+                char prev = s[i - 1];
+                if (curr == '_') _Strb.Append(' '); // Replace underscores with spaces
+                else {
+                    // Add spaces before numbers and uppercase letters
+                    if (curr != ' ' && (char.IsUpper(curr) && (prev != ' ' && prev != '_') || char.IsNumber(curr) && prev != ' ' && prev != '_' && !char.IsNumber(prev))) _Strb.Append(' ');
+                    _Strb.Append(curr);
+                }
+            }
+            return _Strb.ToString();
+        }
+
         /// <summary>
         /// If the given string is a directory path, returns its parent
         /// with or without final slash depending on the original directory format
@@ -99,7 +179,14 @@ namespace DG.DemiEditor
             }
         }
 
+        #endregion
+
         #region Helpers
+
+        static int HexToInt(char hexVal)
+        {
+            return int.Parse(hexVal.ToString(), System.Globalization.NumberStyles.HexNumber);
+        }
 
         // Returns the last index of any slash occurrence, either \ or /
         static int LastIndexOfAnySlash(this string str)
