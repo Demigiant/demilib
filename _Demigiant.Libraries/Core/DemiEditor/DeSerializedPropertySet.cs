1f1ad3d856d184e77bc98778104e1e8faefa021d
Daniele Giardini <daniele.giardini@gmail.com>
2021-07-22 16:18:03 +0200

    [CORE] Better version of DeSerializedPropertySet, working both extended than as-is

--- _Demigiant.Libraries/Core/DemiEditor/DeSerializedPropertySet.cs
+++ _Demigiant.Libraries/Core/DemiEditor/DeSerializedPropertySet.cs
@@ -1,6 +1,8 @@
 ﻿// Author: Daniele Giardini - http://www.demigiant.com
 // Created: 2021/07/09
 
+using System;
+using System.Collections.Generic;
 using System.Reflection;
 using UnityEditor;
 using UnityEngine;
@@ -8,39 +10,63 @@ using UnityEngine;
 namespace DG.DemiEditor
 {
     /// <summary>
-    /// Utility class meant to be extended so you can add as many public fields as the SerializedProperties you want to access.
-    /// They should have the same name as the original property so that calling <see cref="Init"/> will initialize them correctly
+    /// Utility class. You can either use it as is via its constructor, which automatically retrieves all serializedProperties in the instance,
+    /// or you can extend it so you can add as many public SerializedProperties as the SerializedProperties you want to access
+    /// (their name must be the same as the serialized field they refer to)
     /// </summary>
     public class DeSerializedPropertySet
     {
-        bool _initialized;
-        FieldInfo[] _fInfos;
+        readonly List<SerializedProperty> _props = new List<SerializedProperty>();
 
-        /// <summary>
-        /// Initialized the <see cref="SerializedProperty"/> fields, required before any other operation/access
-        /// </summary>
-        /// <param name="so"></param>
-        public void Init(SerializedObject so)
+        public DeSerializedPropertySet(SerializedObject serializedObject)
         {
-            if (_initialized) return;
-
-            _initialized = true;
-
-            _fInfos = this.GetType().GetFields(BindingFlags.Instance | BindingFlags.Public);
-            foreach (FieldInfo f in _fInfos) {
-                if (f.FieldType != typeof(SerializedProperty)) continue;
-                f.SetValue(this, so.FindProperty(f.Name));
-            }
+            if (this.GetType() != typeof(DeSerializedPropertySet)) AutoFillFromChildClassFields(serializedObject);
+            else AutoFillFromSerializedObjectFields(serializedObject);
         }
+
+        #region Public Methods
 
         /// <summary>
         /// Draws all property fields
         /// </summary>
         public void DrawAllPropertyFields()
         {
-            foreach (FieldInfo fi in _fInfos) {
-                EditorGUILayout.PropertyField(fi.GetValue(this) as SerializedProperty);
+            foreach (SerializedProperty sp in _props) EditorGUILayout.PropertyField(sp);
+        }
+
+        #endregion
+
+        #region Methods
+
+        // Used when using this class directly without extending it
+        private void AutoFillFromSerializedObjectFields(SerializedObject serializedObject)
+        {
+            Type t = serializedObject.targetObject.GetType();
+            FieldInfo[] fInfos = t.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            foreach (FieldInfo f in fInfos) {
+                bool isSerialized = f.IsPublic && !f.IsDefined(typeof(NonSerializedAttribute), false)
+                                    || f.IsDefined(typeof(SerializeField), false);
+                if (!isSerialized) continue;
+                SerializedProperty prop = serializedObject.FindProperty(f.Name);
+                if (prop == null) continue;
+                _props.Add(serializedObject.FindProperty(f.Name));
             }
         }
+
+        // Used from class that extend this method to implement SerializedProperties by name
+        private void AutoFillFromChildClassFields(SerializedObject serializedObject)
+        {
+            Type t = this.GetType();
+            FieldInfo[] fInfos = t.GetFields(BindingFlags.Instance | BindingFlags.Public);
+            foreach (FieldInfo f in fInfos) {
+                if (f.FieldType != typeof(SerializedProperty)) continue;
+                SerializedProperty prop = serializedObject.FindProperty(f.Name);
+                if (prop == null) continue;
+                f.SetValue(this, prop);
+                _props.Add(prop);
+            }
+        }
+
+        #endregion
     }
 }
